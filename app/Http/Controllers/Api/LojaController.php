94b43c46daee70eecf8a624b3fec0a290fa4bd1a
Angelo Ricardo <angeloengcomp@gmail.com>
2023-03-13 23:00:21 -0300

    feat: add Loja crud

--- app/Http/Controllers/Api/LojaController.php
+++ app/Http/Controllers/Api/LojaController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Loja;
 use Illuminate\Http\Request;
 
 class LojaController extends Controller
 {
+    private $loja;
+
+    public function __construct(Loja $loja)
+    {
+        $this->loja = $loja;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return $this->loja->paginate(10);
     }
 
     /**
@@ -20,30 +28,70 @@ class LojaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return $this->loja->create($request->all());
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    // public function show(string $id)
+    // {
+    //     try {
+    //         //retorna a loja com os produtos
+    //         return $this->loja->with('produtos')->findOrFail($id);
+    //     } catch (\Exception $e) {
+    //         return response()->json(
+    //             ['error' => true, 'message' => 'Loja não encontrada'],
+    //             404
+    //         );
+    //     }
+    // }
+    // route model binding
+    public function show(Loja $loja)
     {
-        //
+        try {
+            //retorna a loja com os produtos
+            return $loja->with('produtos')->findOrFail($loja->id);
+        } catch (\Exception $e) {
+            return response()->json(
+                ['error' => true, 'message' => 'Loja não encontrada'],
+                404
+            );
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Loja $loja)
     {
-        //
+        try {
+            $loja->update($request->all());
+            return $loja;
+        } catch (\Exception $e) {
+            return response()->json(
+                ['error' => true, 'message' => 'Loja não encontrada'],
+                404
+            );
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Loja $loja)
     {
-        //
+        try {
+            $loja->delete();
+            return response()->json(
+                ['error' => false, 'message' => 'Loja removida com sucesso'],
+                200
+            );
+        } catch (\Exception $e) {
+            return response()->json(
+                ['error' => true, 'message' => 'Loja não encontrada'],
+                404
+            );
+        }
     }
 }
